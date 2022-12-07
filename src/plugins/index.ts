755669974d357003ec534c294952d065339bcbcb
psecuresystem <92157374+psecuresystem@users.noreply.github.com>
2022-12-07 10:12:54 +0000

    Updated plugins file

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,29 +1,20 @@
-import hapiInert from '@hapi/inert';
-import hapiSwagger from 'hapi-swagger';
-import Pack from '../../package.json';
-import hapiVision from '@hapi/vision';
-import { isDev } from '../config/config';
+import hapiInert from "@hapi/inert";
+import * as HapiSwagger from "hapi-swagger";
+import Pack from "../../package.json";
+import hapiVision from "@hapi/vision";
+import Hapi from "@hapi/hapi";
 
-const swaggerOptions = {
-  schemes: [isDev ? 'http' : 'https'],
-  info: { title: 'HighTable Service', version: Pack.version },
-  jsonEditor: true,
-  documentationPath: '/docs',
-  securityDefinitions: {
-    jwt: {
-      type: 'apiKey',
-      name: 'Authorization',
-      in: 'header',
-    },
-  },
-  security: [{ jwt: [] }],
+const swaggerOptions: HapiSwagger.RegisterOptions = {
+  schemes: ["http", "https"],
+  info: { title: "HighTable Service", version: Pack.version },
+  documentationPath: "/docs",
 };
 
-const plugins = [
+const plugins: Array<Hapi.ServerRegisterPluginObject<any>> = [
   { plugin: hapiInert },
   { plugin: hapiVision },
   {
-    plugin: hapiSwagger,
+    plugin: HapiSwagger,
     options: swaggerOptions,
   },
 ];
